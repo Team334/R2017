aa012754cb355de36df7498f30ea7e068e78a19f
August <augustt198@gmail.com>
2017-02-11 00:10:35 -0500

    Add ManualAutonSelect utility

---
+++ src/org/usfirst/frc/team334/robot/util/ManualAutonSelect.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team334.robot.util;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+/**
+ * Class for manually selecting auton mode via toggle
+ * switches on the robot. This is good for acting as
+ * a fallback in case there are issues communicating
+ * with the driver station.
+ */
+public class ManualAutonSelect {
+
+    // channels for the inputs
+    private final static int DI_1_CHANNEL = 0,
+                             DI_2_CHANNEL = 1,
+                             DI_3_CHANNEL = 2;
+
+    private DigitalInput di1, di2, di3;
+
+    public ManualAutonSelect() {
+        this.di1 = new DigitalInput(DI_1_CHANNEL);
+        this.di2 = new DigitalInput(DI_2_CHANNEL);
+        this.di3 = new DigitalInput(DI_3_CHANNEL);
+    }
+
+    /**
+     * Get the auton selection
+     * @return an integer between 0 and 7
+     */
+    public int getSelection() {
+        int bit1 = di1.get() ? 1 : 0;
+        int bit2 = di2.get() ? 1 : 0;
+        int bit3 = di3.get() ? 1 : 0;
+
+        int selection = bit1 | (bit2 << 1) | (bit3 << 2);
+        return selection;
+    }
+}

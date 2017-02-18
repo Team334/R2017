f6937c77701ef47c206c7bdfbaef73e0ed4c7298
DanShafman <dshafman0900@bths.edu>
2017-02-18 17:22:30 -0500

    Made variables private + fixed casting error

--- src/org/usfirst/frc/team334/robot/components/CameraSet.java
+++ src/org/usfirst/frc/team334/robot/components/CameraSet.java
@@ -1,6 +1,7 @@
-package org.usfirst.frc.team334.robot;
+package org.usfirst.frc.team334.robot.components;
 
 import org.opencv.core.Mat;
+import org.usfirst.frc.team334.robot.controls.Controls;
 
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
@@ -10,49 +11,49 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class CameraSet {
-
+	
 //	Button to switch between cameras
-	int switchButton;
+	private int switchButton;
 //	Multiplier to change resolution, default 160x120
-	double multiplier = 2;
-	boolean isToggled = false;
-
-	Joystick joy;
-
-	UsbCamera cam1;
-	UsbCamera cam2;
-
-    CvSource outputStream = CameraServer.getInstance().putVideo("camera_set", (int)(multiplier * 160), (int)(multiplier * 120));
-    Mat source = new Mat();
-    CvSink cvSink;
-
+	private double multiplier = 2;
+	private boolean isToggled = false;
+	
+	private Controls controls;
+	
+	private UsbCamera cam1;
+	private UsbCamera cam2;
+    
+    private CvSource outputStream = CameraServer.getInstance().putVideo("camera_set", (int)(multiplier * 160), (int)(multiplier * 120));
+    private Mat source = new Mat();
+    private CvSink cvSink;
+    
 //  The default camera is cam1
-    public CameraSet(Joystick joy, int switchButton, String devpath1, String devpath2) {
-    	this.joy = joy;
+    public CameraSet(Controls controls, int switchButton, String devpath1, String devpath2) {
+    	this.controls = controls;
     	this.switchButton = switchButton;
     	this.cam1 = CameraServer.getInstance().startAutomaticCapture("cam1", devpath1);
     	this.cam2 = CameraServer.getInstance().startAutomaticCapture("cam2", devpath2);
-
+    	
     	cam1.setResolution((int)(this.multiplier * 160), (int)(this.multiplier * 120));
 		cam2.setResolution((int)(this.multiplier * 160), (int)(this.multiplier * 120));
-
-		cvSink = CameraServer.getInstance().getVideo(cam1);
-    }
-
-	public void setResolution(double multiplier) {
+    
+		cvSink = CameraServer.getInstance().getVideo(cam1); 
+    }   
+    
+	public void setResolution(int multiplier) {
 		cam1.setResolution((int)(multiplier * 160), (int)(multiplier * 120));
 		cam2.setResolution((int)(multiplier * 160), (int)(multiplier * 120));
 	}
-
+	
 	public void enable() {
 		new Thread(() -> {
 			while(!Thread.interrupted()) {
 				SmartDashboard.putString("current camera", cvSink.getSource().getName());
-
+				
 				cvSink.grabFrame(source);
 				outputStream.putFrame(source);
-
-				if(joy.getRawButton(switchButton)) {
+				
+				if(controls.getToggleCamera()) {
 					if(!isToggled) {
 						isToggled = !isToggled;
 						cvSink = CameraServer.getInstance().getVideo(cam2);

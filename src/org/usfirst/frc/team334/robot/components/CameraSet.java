067e9189d82a80673e9da8718450fe5d8f7df2d0
eric <ericjiang378@gmail.com>
2017-02-20 09:49:47 -0500

    Reformat camera set and change toggle

--- src/org/usfirst/frc/team334/robot/components/CameraSet.java
+++ src/org/usfirst/frc/team334/robot/components/CameraSet.java
@@ -7,64 +7,59 @@ import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class CameraSet {
-	
-//	Button to switch between cameras
-	private int switchButton;
-//	Multiplier to change resolution, default 160x120
-	private double multiplier = 2;
-	private boolean isToggled = false;
-	
-	private Controls controls;
-	
-	private UsbCamera cam1;
-	private UsbCamera cam2;
-    
-    private CvSource outputStream = CameraServer.getInstance().putVideo("camera_set", (int)(multiplier * 160), (int)(multiplier * 120));
+
+    //	Button to switch between cameras
+    private int switchButton;
+    //	Multiplier to change resolution, default 160x120
+    private double multiplier = 2;
+    private boolean isToggled = false;
+
+    private Controls controls;
+
+    private UsbCamera cam1;
+    private UsbCamera cam2;
+
+    private CvSource outputStream = CameraServer.getInstance().putVideo("camera_set", (int) (multiplier * 160), (int) (multiplier * 120));
     private Mat source = new Mat();
     private CvSink cvSink;
-    
-//  The default camera is cam1
+
+    //  The default camera is cam1
     public CameraSet(Controls controls, int switchButton, String devpath1, String devpath2) {
-    	this.controls = controls;
-    	this.switchButton = switchButton;
-    	this.cam1 = CameraServer.getInstance().startAutomaticCapture("cam1", devpath1);
-    	this.cam2 = CameraServer.getInstance().startAutomaticCapture("cam2", devpath2);
-    	
-    	cam1.setResolution((int)(this.multiplier * 160), (int)(this.multiplier * 120));
-		cam2.setResolution((int)(this.multiplier * 160), (int)(this.multiplier * 120));
-    
-		cvSink = CameraServer.getInstance().getVideo(cam1); 
-    }   
-    
-	public void setResolution(int multiplier) {
-		cam1.setResolution((int)(multiplier * 160), (int)(multiplier * 120));
-		cam2.setResolution((int)(multiplier * 160), (int)(multiplier * 120));
-	}
-	
-	public void enable() {
-		new Thread(() -> {
-			while(!Thread.interrupted()) {
-				SmartDashboard.putString("current camera", cvSink.getSource().getName());
-				
-				cvSink.grabFrame(source);
-				outputStream.putFrame(source);
-				
-				if(controls.getToggleCamera()) {
-					if(!isToggled) {
-						isToggled = !isToggled;
-						cvSink = CameraServer.getInstance().getVideo(cam2);
-					}
-				} else {
-					if(isToggled) {
-						isToggled = !isToggled;
-						cvSink = CameraServer.getInstance().getVideo(cam1);
-					}
-				}
-			}
-		}).start();
-	}
+        this.controls = controls;
+        this.switchButton = switchButton;
+        this.cam1 = CameraServer.getInstance().startAutomaticCapture("cam1", devpath1);
+        this.cam2 = CameraServer.getInstance().startAutomaticCapture("cam2", devpath2);
+
+        cam1.setResolution((int) (this.multiplier * 160), (int) (this.multiplier * 120));
+        cam2.setResolution((int) (this.multiplier * 160), (int) (this.multiplier * 120));
+
+        cvSink = CameraServer.getInstance().getVideo(cam1);
+    }
+
+    public void setResolution(int multiplier) {
+        cam1.setResolution(multiplier * 160, multiplier * 120);
+        cam2.setResolution(multiplier * 160, multiplier * 120);
+    }
+
+    public void enable() {
+        new Thread(() -> {
+            while (!Thread.interrupted()) {
+                SmartDashboard.putString("current camera", cvSink.getSource().getName());
+
+                cvSink.grabFrame(source);
+                outputStream.putFrame(source);
+
+                if (controls.getToggleCamera() && !isToggled) {
+                    isToggled = true;
+                    cvSink = CameraServer.getInstance().getVideo(cam2);
+                } else if (controls.getToggleCamera() && isToggled) {
+                    isToggled = false;
+                    cvSink = CameraServer.getInstance().getVideo(cam1);
+                }
+            }
+        }).start();
+    }
 }
